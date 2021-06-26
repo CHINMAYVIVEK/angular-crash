b9a0a60992a4fad6245fd7590070f065f612106b
Chinmay Vivek <chinmay.vivek@gmail.com>
2021-06-26 09:43:09 +0530

    [INIT]: Mock Task initialized

---
+++ src/app/mock-task.ts
@@ -0,0 +1,22 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Doctors Appointment',
+    day: 'June 26th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Meeting at School',
+    day: 'June 27th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Food Shopping',
+    day: 'June  28th at 12:30pm',
+    reminder: false,
+  },
+];
